c3a3180893ef0f7a62bac8120ce0e5256c883768
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-12-06 15:17:51 -0600

    Organization

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
 
 export const Navbar = () => {
     return (
@@ -13,7 +13,10 @@ export const Navbar = () => {
                 </Link>
             </div>
             <SearchInput />
-            <UserButton />
+            <div className="flex gap-3 items-center pl-6">
+                <OrganizationSwitcher />
+                <UserButton />
+            </div>
         </nav>
     );
 };
