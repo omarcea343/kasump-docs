c9ec87e7f8413c525e9cdd9b645322990d9020b1
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-12-06 13:23:20 -0600

    pagination

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -10,6 +10,7 @@ import {
     TableRow,
 } from "@/components/ui/table";
 import { DocumentRow } from "./document-row";
+import { Button } from "@/components/ui/button";
 
 interface DocumentTableProps {
     documents: Doc<"documents">[] | undefined;
@@ -54,6 +55,16 @@ export const DocumentsTable = ({ documents, loadMore, status }: DocumentTablePro
                     )}
                 </Table>
             )}
+            <div className="flex items-center justify-center">
+                <Button
+                    variant="ghost"
+                    size="sm"
+                    onClick={() => loadMore(5)}
+                    disabled={status !== "CanLoadMore"}
+                >
+                    {status === "CanLoadMore" ? "Load more" : "End of results"}
+                </Button>
+            </div>
         </div>
     );
 };
