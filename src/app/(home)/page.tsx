879befad1976dc120f1ed0f5f4a600480508d68c
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-12-05 19:15:06 -0600

    Templates

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,14 @@
-import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
     return (
         <div className="min-h-screen flex flex-col">
-            <div className="fixed top-0 left-0 right-0 z-10 bg-white p-4">
+            <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
                 <Navbar />
             </div>
-            <div className="mt-20">
-                Click
-                <Link href="/documents/123">
-                    <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
-                </Link>
-                to go to document id
+            <div className="mt-16">
+                <TemplatesGallery />
             </div>
         </div>
     );
